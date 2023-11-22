e09c425e59cacb12464d1b3aa93351a7a1fac6d3
Rui Melo <viper5000@viper5000.net>
2023-11-22 12:36:56 +0000

    A tall, dark stranger will have more fun than you.

--- converter.py
+++ converter.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 import unicodedata
 
+# Vamos primeiro verificar a codificação do arquivo 'pt_pt.dic'
+import chardet
+
 ENTRADA = 'pt_pt.dic'
+
 SAIDA = 'palavras.txt'
+
+# Detectar a codificação do arquivo
+with open(ENTRADA, 'rb') as file:
+    raw_data = file.read()
+    encoding_result = chardet.detect(raw_data)
+
+encoding_result
+
 
 def normalizar(txt):
     """Remove acentos e transforma em minúsculas."""
@@ -16,8 +28,8 @@ def chave(txt):
     return (normalizar(txt), tuple(ord(c) for c in txt))
 
 palavras = set()
-with open(ENTRADA, encoding='latin-1') as entrada:
-    next(entrada)  # Pular a primeira linha
+with open(ENTRADA, encoding='utf-8') as entrada:
+    next(entrada)  # Saltar a primeira linha
     for linha in entrada:
         palavra_base = linha.strip().split('/')[0]
         partes = palavra_base.split('-')
@@ -25,7 +37,7 @@ with open(ENTRADA, encoding='latin-1') as entrada:
         if len(sufixo) != 2 or sufixo.upper() != sufixo:
             palavras.update(parte for parte in partes if parte)
 
-qt_original = sum(1 for _ in open(ENTRADA, encoding='latin-1')) - 1  # Contando as linhas excluindo a primeira
+qt_original = sum(1 for _ in open(ENTRADA, encoding='utf-8')) - 1  # Conta as linhas excluindo a primeira
 extra = len(palavras) - qt_original
 print(f'{qt_original} palavras na lista original, {len(palavras)} na lista gerada: {extra} adicionadas')
 
